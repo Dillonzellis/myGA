f314d0d817cace183b4d76ec2a4a510bd698056d
Dillon Ellis <dillonzellis@gmail.com>
2022-09-06 12:33:53 -0400

    new styles for slcr

--- calculators/standard_loan/slca.js
+++ calculators/standard_loan/slca.js
@@ -1,151 +1,150 @@
 // **************************************************************
-// InternetActive Calculators™
+// InternetActive Calculatorsï¿½
 // Version 2.0
-// Copyright © 1998 - 2001 Desert Cactus Software Corporation
+// Copyright ï¿½ 1998 - 2001 Desert Cactus Software Corporation
 // **************************************************************
 
-	var ZZZ = getValue(passed, 'calculate_option');
-	if (ZZZ == 1) {
+var ZZZ = getValue(passed, "calculate_option");
+if (ZZZ == 1) {
+  document.write("<CENTER>");
+  document.write("<TABLE Cellpadding=10 class='amort-table'>");
 
-	document.write("<CENTER>");
-	document.write("<TABLE Border=1 Cellpadding=7>");
+  document.write("<TR>");
+  document.write("<TD COLSPAN=4 class='main-header'");
+  document.write("<span class=FontTwo>");
+  document.write("<CENTER>");
+  document.write("<STRONG>Amortization Schedule</STRONG>");
+  document.write("</CENTER>");
+  document.write("</span>");
+  document.write("</TD>");
+  document.write("</TR>");
 
-	document.write("<TR>");
-	document.write("<TD COLSPAN=4 BGCOLOR=#FFFFEE>");
-	document.write("<span class=FontTwo>");
-	document.write("<CENTER>");
-	document.write("<STRONG>Amortization Schedule</STRONG>");
-	document.write("</CENTER>");
-	document.write("</span>");
-	document.write("</TD>");
-	document.write("</TR>");
+  document.write("<TR>");
+  document.write("<TD BGCOLOR=#EEEEEE>");
+  document.write("<span class=FontFour>");
+  document.write("<CENTER>");
+  document.write("<STRONG>Payment</STRONG>");
+  document.write("</CENTER>");
+  document.write("</span>");
+  document.write("</TD>");
 
-	document.write("<TR>");
-	document.write("<TD BGCOLOR=#EEEEEE>");
-	document.write("<span class=FontFour>");
-	document.write("<CENTER>");
-	document.write("<STRONG>Payment</STRONG>");
-	document.write("</CENTER>");
-	document.write("</span>");
-	document.write("</TD>");
-		
-	document.write("<TD BGCOLOR=#EEEEEE>");
-	document.write("<span class=FontFour>");
-	document.write("<CENTER>");
-	document.write("<STRONG>Principal</STRONG>");
-	document.write("</CENTER>");
-	document.write("</span>");
-	document.write("</TD>");
-		
-	document.write("<TD BGCOLOR=#EEEEEE>");
-	document.write("<span class=FontFour>");
-	document.write("<CENTER>");
-	document.write("<STRONG>Interest</STRONG>");
-	document.write("</CENTER>");
-	document.write("</span>");
-	document.write("</TD>");
-		
-	document.write("<TD BGCOLOR=#EEEEEE>");
-	document.write("<span class=FontFour>");
-	document.write("<CENTER>");
-	document.write("<STRONG>Loan Balance</STRONG>");
-	document.write("</CENTER>");
-	document.write("</span>");
-	document.write("</TD>");
-	document.write("</TR>");
+  document.write("<TD BGCOLOR=#EEEEEE>");
+  document.write("<span class=FontFour>");
+  document.write("<CENTER>");
+  document.write("<STRONG>Principal</STRONG>");
+  document.write("</CENTER>");
+  document.write("</span>");
+  document.write("</TD>");
 
-	document.write("<TR>");
-	document.write("<TD BGCOLOR=#FFFFFF>");
-	document.write("<span class=FontFour>");
-	document.write("<CENTER>");
-	document.write("-");
-	document.write("</CENTER>");
-	document.write("</span>");
-	document.write("</TD>");
-		
-	document.write("<TD BGCOLOR=#FFFFFF>");
-	document.write("<span class=FontFour>");
-	document.write("<CENTER>");
-	document.write("-");
-	document.write("</CENTER>");
-	document.write("</span>");
-	document.write("</TD>");
-		
-	document.write("<TD BGCOLOR=#EEFFEE>");
-	document.write("<span class=FontFour>");
-	document.write("<CENTER>");
-	document.write(""+roundingPad((B*100.00)/100)+"%");
-	document.write("</CENTER>");
-	document.write("</span>");
-	document.write("</TD>");
-		
-	document.write("<TD BGCOLOR=#EEFFEE>");
-	document.write("<span class=FontFour>");
-	document.write("<CENTER>");
-	document.write(currencyPad(A));
-	document.write("</CENTER>");
-	document.write("</span>");
-	document.write("</TD>");
-	document.write("</TR>");
-		
-	var KK = 0;
-	var JJ = 0;
-	var HH = 0;
-						
-	for (var I = 1; I <= TOTPER; I++) {
+  document.write("<TD BGCOLOR=#EEEEEE>");
+  document.write("<span class=FontFour>");
+  document.write("<CENTER>");
+  document.write("<STRONG>Interest</STRONG>");
+  document.write("</CENTER>");
+  document.write("</span>");
+  document.write("</TD>");
 
-		HH = I;
-			
-		KK = INT * A;
+  document.write("<TD BGCOLOR=#EEEEEE>");
+  document.write("<span class=FontFour>");
+  document.write("<CENTER>");
+  document.write("<STRONG>Loan Balance</STRONG>");
+  document.write("</CENTER>");
+  document.write("</span>");
+  document.write("</TD>");
+  document.write("</TR>");
 
-		JJ = PMT - KK;
-					
-		A = A - JJ;
-			
-		if (A <= .25) { A = 0 }
-		if (KK <= 0) { A = 0 }
+  document.write("<TR>");
+  document.write("<TD bgcolor=#eee>");
+  document.write("<span class=FontFour>");
+  document.write("<CENTER>");
+  document.write("-");
+  document.write("</CENTER>");
+  document.write("</span>");
+  document.write("</TD>");
 
-	document.write("<TR BGCOLOR=#EEFFEE>");
-	document.write("<TD>");
-	document.write("<span class=FontFour>");
-	document.write("<CENTER>");
-	document.write(HH);
-	document.write("</CENTER>");
-	document.write("</span>");
-	document.write("</TD>");
-		
-	document.write("<TD>");
-	document.write("<span class=FontFour>");
-	document.write("<CENTER>");
-	document.write(currencyPad(JJ));
-	document.write("</CENTER>");
-	document.write("</span>");
-	document.write("</TD>");
-		
-	document.write("<TD>");
-	document.write("<span class=FontFour>");
-	document.write("<CENTER>");
-	document.write(currencyPad(KK));
-	document.write("</CENTER>");
-	document.write("</span>");
-	document.write("</TD>");
-		
-	document.write("<TD>");
-	document.write("<span class=FontFour>");
-	document.write("<CENTER>");
-	document.write(currencyPad(A));
-	document.write("</CENTER>");
-	document.write("</span>");
-	document.write("</TD>");
-	document.write("</TR>");
+  document.write("<TD>");
+  document.write("<span class=FontFour>");
+  document.write("<CENTER>");
+  document.write("-");
+  document.write("</CENTER>");
+  document.write("</span>");
+  document.write("</TD>");
 
-	}
+  document.write("<TD>");
+  document.write("<span class=FontFour>");
+  document.write("<CENTER>");
+  document.write("" + roundingPad((B * 100.0) / 100) + "%");
+  document.write("</CENTER>");
+  document.write("</span>");
+  document.write("</TD>");
 
-	document.write("</TABLE>");
-	document.write("<BR>");
-	document.write("<BR>");
+  document.write("<TD>");
+  document.write("<span class=FontFour>");
+  document.write("<CENTER>");
+  document.write(currencyPad(A));
+  document.write("</CENTER>");
+  document.write("</span>");
+  document.write("</TD>");
+  document.write("</TR>");
 
-	}
+  var KK = 0;
+  var JJ = 0;
+  var HH = 0;
+
+  for (var I = 1; I <= TOTPER; I++) {
+    HH = I;
+
+    KK = INT * A;
+
+    JJ = PMT - KK;
+
+    A = A - JJ;
+
+    if (A <= 0.25) {
+      A = 0;
+    }
+    if (KK <= 0) {
+      A = 0;
+    }
+
+    document.write("<TR BGCOLOR=#eee>");
+    document.write("<TD>");
+    document.write("<span class=FontFour>");
+    document.write("<CENTER>");
+    document.write(HH);
+    document.write("</CENTER>");
+    document.write("</span>");
+    document.write("</TD>");
+
+    document.write("<TD>");
+    document.write("<span class=FontFour>");
+    document.write("<CENTER>");
+    document.write(currencyPad(JJ));
+    document.write("</CENTER>");
+    document.write("</span>");
+    document.write("</TD>");
+
+    document.write("<TD>");
+    document.write("<span class=FontFour>");
+    document.write("<CENTER>");
+    document.write(currencyPad(KK));
+    document.write("</CENTER>");
+    document.write("</span>");
+    document.write("</TD>");
+
+    document.write("<TD>");
+    document.write("<span class=FontFour>");
+    document.write("<CENTER>");
+    document.write(currencyPad(A));
+    document.write("</CENTER>");
+    document.write("</span>");
+    document.write("</TD>");
+    document.write("</TR>");
+  }
+
+  document.write("</TABLE>");
+  document.write("<BR>");
+  document.write("<BR>");
+}
 
 // **************************************************************
-
