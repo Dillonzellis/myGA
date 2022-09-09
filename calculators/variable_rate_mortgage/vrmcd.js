cb94865728e612af6c8de77fa6392b232f8989a0
Dillon Ellis <dillonzellis@gmail.com>
2022-09-09 09:16:36 -0400

    variable rate mort calc styled

--- calculators/variable_rate_mortgage/vrmcd.js
+++ calculators/variable_rate_mortgage/vrmcd.js
@@ -1,7 +1,7 @@
 // **************************************************************
-// InternetActive Calculators™
+// InternetActive Calculatorsï¿½
 // Version 2.0
-// Copyright © 1998 - 2001 Desert Cactus Software Corporation
+// Copyright ï¿½ 1998 - 2001 Desert Cactus Software Corporation
 // **************************************************************
 
 // (1) Initialize interest rate for each six-month period.
@@ -9,34 +9,31 @@
 //     per year).
 // (3) Calculate payment for each six-month period.
 
+for (I = 1; I <= NORC - 1; I++) {
+  B = SR + I * IR;
+  var INT = B / 1200;
+  var NUM = A * INT;
+  var DEN = 1 - 1 / Math.pow(1 + INT, TOTPER);
+  var PMT = NUM / DEN;
 
-	for (I = 1; I <= NORC - 1; I++)
-		{
-			B = SR + (I * IR);
-			var INT = B / 1200;
-			var NUM = A * INT;
-			var DEN = 1 - (1 / Math.pow(1 + INT, TOTPER));
-			var PMT = (NUM / DEN);
-						
-			document.write("<TR>");
-			document.write("<TD BGCOLOR=#EEEEEE>");
-			document.write("<span class=FontFour>");
-			document.write("Monthly Payment (next six months):");
-			document.write("</span>");
-			document.write("</TD>");
-			document.write("<TD BGCOLOR=#EEFFEE>");
-			document.write("<span class=FontFour>");
-			document.write(currencyPad(PMT));
-			document.write("</span>");
-			document.write("</TD>");
-			document.write("</TR>");
-		}
-
+  document.write("<TR>");
+  document.write("<TD class='left-col'>");
+  document.write("<span class=FontFour>");
+  document.write("Monthly Payment (next six months):");
+  document.write("</span>");
+  document.write("</TD>");
+  document.write("<TD class='left-col'>");
+  document.write("<span class=FontFour>");
+  document.write(currencyPad(PMT));
+  document.write("</span>");
+  document.write("</TD>");
+  document.write("</TR>");
+}
 
 // **************************************************************
 
 // Total amount in payments, thus far.
 
-	var TOTPMT = 6 * PMT;
+var TOTPMT = 6 * PMT;
 
 // **************************************************************
