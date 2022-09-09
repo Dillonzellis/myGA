8d28f817254166f21e54fe4d2eb08e4b78d618f6
Dillon Ellis <dillonzellis@gmail.com>
2022-09-09 09:42:24 -0400

    compund interest calc styled

--- calculators/compound_interest/read.js
+++ calculators/compound_interest/read.js
@@ -1,24 +1,22 @@
 // **************************************************************
-// InternetActive Calculators™
+// InternetActive Calculatorsï¿½
 // Version 2.0
-// Copyright © 1998 - 2001 Desert Cactus Software Corporation
+// Copyright ï¿½ 1998 - 2001 Desert Cactus Software Corporation
 // **************************************************************
 
-var DCS = 0
+var DCS = 0;
 
-DCS = "Copyright &copy; 1998 - 2001 Desert Cactus Software Corporation."
+// DCS = "Copyright &copy; 1998 - 2001 Desert Cactus Software Corporation."
 
-function getValue(string,value) {
-
-	var startPos = string.indexOf(value + "=");
-	if (startPos > -1) {
-		startPos = startPos + value.length + 1;
-		var endPos = string.indexOf("&",startPos);
-		if (endPos == -1)
-			endPos = string.length;
-		return unescape(string.substring(startPos,endPos));
-	}
-	return '';
+function getValue(string, value) {
+  var startPos = string.indexOf(value + "=");
+  if (startPos > -1) {
+    startPos = startPos + value.length + 1;
+    var endPos = string.indexOf("&", startPos);
+    if (endPos == -1) endPos = string.length;
+    return unescape(string.substring(startPos, endPos));
+  }
+  return "";
 }
 
 var passed = location.search.substring(1);
