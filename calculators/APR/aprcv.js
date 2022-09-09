dfe49263827bcb747a36144a2e526a107ee34f40
Dillon Ellis <dillonzellis@gmail.com>
2022-09-09 09:49:25 -0400

    apr calc styled

--- calculators/APR/aprcv.js
+++ calculators/APR/aprcv.js
@@ -1,15 +1,17 @@
 // **************************************************************
-// InternetActive Calculators™
+// InternetActive Calculatorsï¿½
 // Version 2.0
-// Copyright © 1998 - 2001 Desert Cactus Software Corporation
+// Copyright ï¿½ 1998 - 2001 Desert Cactus Software Corporation
 // **************************************************************
 
 // Help window.
 
 function help(form) {
-
-	window.open("aprchelp.html","newWindow","toolbar=yes,location=0,directories=yes,status=no,menubar=1,scrollbars=yes,resizable=0,copyhistory=1,width=300,height=200,top=200,left=450");
-
+  window.open(
+    "aprchelp.html",
+    "newWindow",
+    "toolbar=yes,location=0,directories=yes,status=no,menubar=1,scrollbars=yes,resizable=0,copyhistory=1,width=300,height=200,top=200,left=450"
+  );
 }
 
 // **************************************************************
@@ -17,38 +19,37 @@ function help(form) {
 // Verification function for loan amount.
 
 function checkLA(form) {
+  function checkNumber(input, msg) {
+    var M = form.LoanAmount.value;
 
-	function checkNumber(input, msg) {
+    for (var I = 0; I < M.length; I++) {
+      var J = M.substring(I, I + 1);
+      if ((J < "0" || J > "9") && J != "." && J != "") {
+        alert(msg);
+        return false;
+      }
+    }
 
-		var M = form.LoanAmount.value;
+    return true;
+  }
 
-			for (var I = 0; I < M.length; I++) {
-				var J = M.substring(I, I + 1);
-				if ((J < "0" || J > "9") && J != "." && J != "") {
-				alert(msg);
-				return false;
-			}
-		}
+  var M = form.LoanAmount.value;
 
-	return true;
+  if (!checkNumber(M, "Please enter a valid numeric expression.")) {
+    form.LoanAmount.focus();
+    form.LoanAmount.select();
+    return;
+  }
 
-	}
-
-	var M = form.LoanAmount.value;
-
-	if (!checkNumber(M, "Please enter a valid numeric expression.")) {
-		form.LoanAmount.focus();
-		form.LoanAmount.select();
-		return;
-	}
-
-	if (M != "") {
-		if ((M < 0) || (M > 1000000000)) {
-			alert("Please enter a numeric value between 0 and 1 billion (a 1 followed by nine zeroes).");
-			form.LoanAmount.focus();
-			form.LoanAmount.select();
-		}
-	}
+  if (M != "") {
+    if (M < 0 || M > 1000000000) {
+      alert(
+        "Please enter a numeric value between 0 and 1 billion (a 1 followed by nine zeroes)."
+      );
+      form.LoanAmount.focus();
+      form.LoanAmount.select();
+    }
+  }
 }
 
 // **************************************************************
@@ -56,38 +57,35 @@ function checkLA(form) {
 // Verification function for interest rate.
 
 function checkIR(form) {
+  function checkNumber(input, msg) {
+    var M = form.InterestRate.value;
 
-	function checkNumber(input, msg) {
+    for (var I = 0; I < M.length; I++) {
+      var J = M.substring(I, I + 1);
+      if ((J < "0" || J > "9") && J != "." && J != "") {
+        alert(msg);
+        return false;
+      }
+    }
 
-		var M = form.InterestRate.value;
+    return true;
+  }
 
-			for (var I = 0; I < M.length; I++) {
-				var J = M.substring(I, I + 1);
-				if ((J < "0" || J > "9") && J != "." && J != "") {
-				alert(msg);
-				return false;
-			}
-		}
+  var M = form.InterestRate.value;
 
-	return true;
+  if (!checkNumber(M, "Please enter a valid numeric expression.")) {
+    form.InterestRate.focus();
+    form.InterestRate.select();
+    return;
+  }
 
-	}
-
-	var M = form.InterestRate.value;
-
-	if (!checkNumber(M, "Please enter a valid numeric expression.")) {
-		form.InterestRate.focus();
-		form.InterestRate.select();
-		return;
-	}
-
-	if (M != "") {
-		if ((M < 0) || (M > 40)) {
-			alert("Please enter a numeric value between 0 and 40.");
-			form.InterestRate.focus();
-			form.InterestRate.select();
-		}
-	}
+  if (M != "") {
+    if (M < 0 || M > 40) {
+      alert("Please enter a numeric value between 0 and 40.");
+      form.InterestRate.focus();
+      form.InterestRate.select();
+    }
+  }
 }
 
 // **************************************************************
@@ -95,120 +93,115 @@ function checkIR(form) {
 // Verification function for number of years.
 
 function checkNY(form) {
+  function checkNumber(input, msg) {
+    var M = form.NumberOfYears.value;
 
-	function checkNumber(input, msg) {
+    for (var I = 0; I < M.length; I++) {
+      var J = M.substring(I, I + 1);
+      if ((J < "0" || J > "9") && J != "." && J != "") {
+        alert(msg);
+        return false;
+      }
+    }
 
-		var M = form.NumberOfYears.value;
+    return true;
+  }
 
-			for (var I = 0; I < M.length; I++) {
-				var J = M.substring(I, I + 1);
-				if ((J < "0" || J > "9") && J != "." && J != "") {
-				alert(msg);
-				return false;
-			}
-		}
+  var M = form.NumberOfYears.value;
 
-	return true;
+  if (!checkNumber(M, "Please enter a valid numeric expression.")) {
+    form.NumberOfYears.focus();
+    form.NumberOfYears.select();
+    return;
+  }
 
-	}
-
-	var M = form.NumberOfYears.value;
-
-	if (!checkNumber(M, "Please enter a valid numeric expression.")) {
-		form.NumberOfYears.focus();
-		form.NumberOfYears.select();
-		return;
-	}	
-
-	if (M != "") {
-		if ((M < 0) || (M > 18250)) {
-			alert("Please enter a numeric value between 0 and 18,250.");
-			form.NumberOfYears.focus();
-			form.NumberOfYears.select();
-		}
-	}
+  if (M != "") {
+    if (M < 0 || M > 18250) {
+      alert("Please enter a numeric value between 0 and 18,250.");
+      form.NumberOfYears.focus();
+      form.NumberOfYears.select();
+    }
+  }
 }
 
 // **************************************************************
 
 // Verification function for prepaid interest.
 
-var DCS = 0
+var DCS = 0;
 
 function checkPPI(form) {
+  function checkNumber(input, msg) {
+    var M = form.PrepaidInterest.value;
 
-	function checkNumber(input, msg) {
+    for (var I = 0; I < M.length; I++) {
+      var J = M.substring(I, I + 1);
+      if ((J < "0" || J > "9") && J != "." && J != "") {
+        alert(msg);
+        return false;
+      }
+    }
 
-		var M = form.PrepaidInterest.value;
+    return true;
+  }
 
-			for (var I = 0; I < M.length; I++) {
-				var J = M.substring(I, I + 1);
-				if ((J < "0" || J > "9") && J != "." && J != "") {
-				alert(msg);
-				return false;
-			}
-		}
+  var M = form.PrepaidInterest.value;
 
-	return true;
+  if (!checkNumber(M, "Please enter a valid numeric expression.")) {
+    form.PrepaidInterest.focus();
+    form.PrepaidInterest.select();
+    return;
+  }
 
-	}
-
-	var M = form.PrepaidInterest.value;
-
-	if (!checkNumber(M, "Please enter a valid numeric expression.")) {
-		form.PrepaidInterest.focus();
-		form.PrepaidInterest.select();
-		return;
-	}
-
-	if (M != "") {
-		if ((M < 0) || (M > 1000000000)) {
-			alert("Please enter a numeric value between 0 and 1 billion (a 1 followed by nine zeroes).");
-			form.PrepaidInterest.focus();
-			form.PrepaidInterest.select();
-		}
-	}
+  if (M != "") {
+    if (M < 0 || M > 1000000000) {
+      alert(
+        "Please enter a numeric value between 0 and 1 billion (a 1 followed by nine zeroes)."
+      );
+      form.PrepaidInterest.focus();
+      form.PrepaidInterest.select();
+    }
+  }
 }
 
 // **************************************************************
 
 // Verification function for lender's fees.
 
-DCS = "Copyright &copy; 1998 - 2000 Desert Cactus Software Corporation."
+// DCS = "Copyright &copy; 1998 - 2000 Desert Cactus Software Corporation."
 
 function checkLF(form) {
+  function checkNumber(input, msg) {
+    var M = form.LendersFees.value;
 
-	function checkNumber(input, msg) {
+    for (var I = 0; I < M.length; I++) {
+      var J = M.substring(I, I + 1);
+      if ((J < "0" || J > "9") && J != "." && J != "") {
+        alert(msg);
+        return false;
+      }
+    }
 
-		var M = form.LendersFees.value;
+    return true;
+  }
 
-			for (var I = 0; I < M.length; I++) {
-				var J = M.substring(I, I + 1);
-				if ((J < "0" || J > "9") && J != "." && J != "") {
-				alert(msg);
-				return false;
-			}
-		}
+  var M = form.LendersFees.value;
 
-	return true;
+  if (!checkNumber(M, "Please enter a valid numeric expression.")) {
+    form.LendersFees.focus();
+    form.LendersFees.select();
+    return;
+  }
 
-	}
-
-	var M = form.LendersFees.value;
-
-	if (!checkNumber(M, "Please enter a valid numeric expression.")) {
-		form.LendersFees.focus();
-		form.LendersFees.select();
-		return;
-	}
-
-	if (M != "") {
-		if ((M < 0) || (M > 1000000000)) {
-			alert("Please enter a numeric value between 0 and 1 billion (a 1 followed by nine zeroes).");
-			form.LendersFees.focus();
-			form.LendersFees.select();
-		}
-	}
+  if (M != "") {
+    if (M < 0 || M > 1000000000) {
+      alert(
+        "Please enter a numeric value between 0 and 1 billion (a 1 followed by nine zeroes)."
+      );
+      form.LendersFees.focus();
+      form.LendersFees.select();
+    }
+  }
 }
 
 // **************************************************************
--- calculators/APR/enda.js
+++ calculators/APR/enda.js
@@ -1,25 +1,28 @@
 // **************************************************************
-// InternetActive Calculators™
+// InternetActive Calculatorsï¿½
 // Version 2.0
-// Copyright © 1998 - 2001 Desert Cactus Software Corporation
+// Copyright ï¿½ 1998 - 2001 Desert Cactus Software Corporation
 // **************************************************************
 
 document.write("<BR>");
-document.write("<INPUT Type=submit Value=Calculate style='font-family: Verdana, Arial; font-size: 8pt'>");
+document.write("<INPUT Type=submit Value=Calculate class='footer-inputs'>");
 document.write("&nbsp;");
-document.write("<INPUT Type=button Value=Reset style='font-family: Verdana, Arial; font-size: 8pt' onClick=reset(this.form);>");
+document.write(
+  "<INPUT Type=button Value=Reset class='footer-inputs' onClick=reset(this.form);>"
+);
 document.write("&nbsp;");
-document.write("<INPUT Type=button Value=Help style='font-family: Verdana, Arial; font-size: 8pt' onClick=help(this.form);>");
+document.write(
+  "<INPUT Type=button Value=Help class='footer-inputs' onClick=help(this.form);>"
+);
 document.write("<BR>");
 document.write("<BR>");
 document.write("</CENTER>");
 document.write("<center>");
 document.write("<span class=FontSeven>");
-document.write(DCS);
+
 document.write("</span>");
 document.write("</center>");
 document.write("</FORM>");
 document.write("</td>");
 document.write("</tr>");
 document.write("</TABLE>");
-
